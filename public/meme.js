74c1e9e37c0c9f809ed609218899d84f178ab451
Glitch (assets-lib) <none>
2020-04-08 23:33:40 +0000

    🔮🏈 Checkpoint ./public/index.html:14079408/533 ./public/meme.js:14079408/484

--- public/meme.js
+++ public/meme.js
@@ -16,3 +16,21 @@ function next() {
       break;
   }
 }
+function last() {
+  if (id == 0) id = 4;
+  id = (id - 1) % 4;
+  switch (id) {
+    case 0:
+      document.getElementById("img").src = "./assets/fabric.jpg";
+      break;
+    case 1:
+      document.getElementById("img").src = "./assets/fatherPortrait.jpg";
+      break;
+    case 2:
+      document.getElementById("img").src = "./assets/openAfterDeath.jpg";
+      break;
+    case 3:
+      document.getElementById("img").src = "./assets/roomCube.jpg";
+      break;
+  }
+}
