f13b3d0e79c19d12337c662f9639a4429d3c681e
Glitch (assets-lib) <none>
2020-04-09 12:25:53 +0000

    🍞👾 Checkpoint ./public/style.css:14079408/67 ./public/script.js:14079408/5290 ./public/index.html:14079408/85

--- public/script.js
+++ public/script.js
@@ -6,15 +6,23 @@ function next() {
   switch (id) {
     case 0:
       document.getElementById("pic").src = "./assets/fabric.jpg";
+      document.getElementById("info").innerHTML =
+        "Stephen Kaltenbach and guests, MODERN DRAPERY, 2020. Felt Fabric, 45 x 60 inches.";
       break;
     case 1:
       document.getElementById("pic").src = "./assets/fatherPortrait.jpg";
+      document.getElementById("info").innerHTML =
+        "Stephen Kaltenbach, Portrait of My Father, 1972-79. Acrylic on canvas, 114 x 170" + '3/4'.split('/') + "inches";
       break;
     case 2:
       document.getElementById("pic").src = "./assets/openAfterDeath.jpg";
+      document.getElementById("info").innerHTML =
+        "Stephen Kaltenbach, Room Cube, 1967. Blueprint, 18 x 24 inches, edition of 10.";
       break;
     case 3:
       document.getElementById("pic").src = "./assets/roomCube.jpg";
+      document.getElementById("info").innerHTML =
+        "Stephen Kaltenbach, OPEN AFTER MY DEATH, 1970. Mild steel, engraved, with unknown contents, 3 x 6 x 3 inches.";
       break;
   }
 }
@@ -41,6 +49,8 @@ function showtime() {
   var nowtime = new Date();
   var endtime = new Date("2020/05/10, 17:00:00");
   var lefttime = parseInt((endtime.getTime() - nowtime.getTime()) / 1000);
-  document.getElementById("countdown").innerHTML = Number(lefttime).toLocaleString();
+  document.getElementById("countdown").innerHTML = Number(
+    lefttime
+  ).toLocaleString();
   setTimeout(showtime, 1000);
 }
