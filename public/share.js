baf67030863622f22c4753837b85bb5d28fa0237
Glitch (weak-playful-winterberry) <none>
2020-05-28 11:42:01 +0000

    🚁💆 Checkpoint ./public/share.js:13964885/1265 ./public/index.html:13964885/7 ./public/style.css:13964885/16 ./public/game.html:13964885/72 ./public/search.html:13964885/67 ./.glitch-assets:13964885/709 ./public/tinder.html:13964885/358

--- public/share.js
+++ public/share.js
@@ -26,42 +26,6 @@ document.querySelector("#butn2").addEventListener("click", () => {
   window.location = "https://weak-playful-winterberry.glitch.me/search.html";
 });
 
-document.querySelector("#forgot").addEventListener("click", () => {
-  var share = document.getElementsByClassName("link");
-  for (var i = 0; i < share.length; i++) {
-    share[i].style.display = null;
-  }
-  var searchbtn = document.getElementById("butn2");
-  searchbtn.style.display = null;
-  var get = document.getElementById("butn3");
-  get.style.display = "none";
-  var enter = document.getElementsByClassName("enter");
-  for (var i = 0; i < enter.length; i++) {
-    enter[i].style.display = "none";
-  }
-  var forgot = document.getElementById("forgot");
-  forgot.style.display = "none";
-});
-
-document.querySelector("#butn3").addEventListener("click", () => {
-  var getbtn = document.getElementById("butn3");
-  getbtn.style.display = "none";
-  var enter = document.getElementsByClassName("enter");
-  for (var i = 0; i < enter.length; i++) {
-    enter[i].style.display = "none";
-  }
-  var forgot = document.getElementById("forgot");
-  forgot.style.display = "none";
-  var resinfo = document.getElementsByClassName("infodisplay");
-  for (var i = 0; i < resinfo.length; i++) {
-    resinfo[i].style.display = null;
-  }
-  var no = document.getElementById("no");
-  no.style.display = null;
-  var yes = document.getElementById("yes");
-  yes.style.display = null;
-});
-
 document.querySelector("#link3").addEventListener("click", () => {
   const url = "https://weak-playful-winterberry.glitch.me/play.html?id=" + id;
   const el = document.createElement("textarea");
