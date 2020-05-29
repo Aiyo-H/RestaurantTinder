95753618e5c3f8101f53b9e22e5e09d25e17af10
Glitch (weak-playful-winterberry) <none>
2020-05-29 02:13:57 +0000

    🏌🐫 Checkpoint ./server.js:14076820/553

--- server.js
+++ server.js
@@ -182,9 +182,33 @@ var mailOptions = {
 /*----------------WebSocket---------------------*/
 
 /*--------Chat box------------*/
-const server = http.createServer(app);
+c*onst server = http.createServer(app);
 
 const wssT = new WebSocket.Server({server});
 
 
-/*--------Game round------------*/
+/wss.on('connection', (ws) => {
+  console.log("a new client");
+  ws.on('message', (message) => {
+    console.log(message);
+    //ws.send("server echo:" + message);
+    broadcast(message)
+  })
+  ws.send('connected!')
+})
+
+function broadcast(data) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      console.log(data);
+      client.send(data);
+    }
+  });
+}
+
+//start our server
+server.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
+*/
+*--------Game round------------*///
