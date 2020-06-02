71912a555dec552d4ce15bd2adbdd50023bbea43
Glitch (weak-playful-winterberry) <none>
2020-06-02 10:20:51 +0000

    ⛽️🍠 Checkpoint ./public/share.js:13964885/594

--- public/share.js
+++ public/share.js
@@ -43,4 +43,21 @@ document.querySelector("#link3").addEventListener("click", () => {
   var body = "Here is the link to join the Game:) " + url;
   window.open('mailto:test@example.com?subject=' + subject + '&body=' + body);
 });
+document.querySelector("#link3").addEventListener("click", () => {
+  const url = "https://weak-playful-winterberry.glitch.me/play.html?id=" + id;
+  const el = document.createElement("textarea");
+  el.value = url;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  console.log("link copied");
+  
+  
+  
+  // Use them in email.onclick()
+  var subject = "Invitation to RestaurantTinder";
+  var body = "Here is the link to join the Game:) " + url;
+  window.open('mailto:test@example.com?subject=' + subject + '&body=' + body);
+});
 
