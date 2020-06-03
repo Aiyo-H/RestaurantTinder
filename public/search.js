0963aa6e1d261d2cef46c24a4924d0e269dd6201
Glitch (weak-playful-winterberry) <none>
2020-06-03 08:23:22 +0000

    🌅✍️ Checkpoint ./public/search.js:14079408/459

--- public/search.js
+++ public/search.js
@@ -6,3 +6,20 @@ document.querySelector("#forgot").addEventListener("click", () => {
 document.querySelector("#butn3").addEventListener("click", () => {
   window.location = "https://weak-playful-winterberry.glitch.me/tinder.html";
 });
+
+
+// -------------------Yelp API-----------------------------
+
+const yelp = require('yelp-fusion');
+const client = yelp.client('YOUR_API_KEY');
+
+client.search({
+  term: 'Four Barrel Coffee',
+  location: 'san francisco, ca',
+}).then(response => {
+  console.log(response.jsonBody.businesses[0].name);
+}).catch(e => {
+  console.log(e);
+});
+
+// ---------------------------------------------------------
