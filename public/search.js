69a82062196b70cb14c751110028c6c4f4992fc2
Glitch (weak-playful-winterberry) <none>
2020-06-03 08:11:24 +0000

    🚣🦀 Checkpoint ./public/search.js:14079408/1620 ./server.js:14079408/182

--- public/search.js
+++ public/search.js
@@ -1,9 +1,4 @@
 "use strict";
-window.onload
-document.querySelector("#butn2").addEventListener("click", () => {
-  window.location = "https://weak-playful-winterberry.glitch.me/search.html";
-});
-
 document.querySelector("#forgot").addEventListener("click", () => {
   window.location = "https://weak-playful-winterberry.glitch.me/game.html";
 });
