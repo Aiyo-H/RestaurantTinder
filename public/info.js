f2fd50268a05164d0f484e0e4d5f0380889563c6
Glitch (weak-playful-winterberry) <none>
2020-06-04 06:38:20 +0000

    🐀🏂 Checkpoint ./public/style.css:13964885/396 ./public/tinder.html:14079408/294 ./public/info.js:14079408/442

--- public/info.js
+++ public/info.js
@@ -6,11 +6,20 @@ window.onload = () => {
     let restaurant = JSON.parse(xhr.responseText);
     data = restaurant.data;
     console.log(data[0].image_url);
+    
+    // Images
     document.getElementById("rest1").src = data[0].image_url;
     document.getElementById("rest2").src = data[1].image_url;
     document.getElementById("rest3").src = data[2].image_url;
     document.getElementById("rest4").src = data[3].image_url;
     document.getElementById("rest5").src = data[4].image_url;
+    
+    // Names
+    document.getElementById("restname1").innerHTML = data[0].name;
+    document.getElementById("restname2").innerHTML  = data[1].name;
+    document.getElementById("restname3").innerHTML  = data[2].name;
+    document.getElementById("restname4").innerHTML  = data[3].name;
+    document.getElementById("restname5").innerHTML  = data[4].name;
   });
   xhr.send(null);
 };
