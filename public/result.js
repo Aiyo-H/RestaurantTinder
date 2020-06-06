b6f5fb231c1cf7cdd2f6cb20ac5b5027f1eef618
Glitch (weak-playful-winterberry) <none>
2020-06-06 11:20:42 +0000

    💷🐗 Checkpoint ./public/result.js:14079408/467

--- public/result.js
+++ public/result.js
@@ -3,13 +3,17 @@ window.onload = () => {
   xhr.open("GET", '/result');
   xhr.addEventListener("load", () => {
     console.log(JSON.parse(xhr.responseText));
-    let 
+    let result = JSON.parse(xhr.responseText);
+    let rests, votes;
     for (var i = 0; i < 8; i++) {
-      //if (removeList.includes(i)) continue;
       
-      // Images
-      img = 'rest' + (i + 1).toString();
-      document.getElementById(img).src = data[i].image_url;
+      // Rests
+      rests = 'result' + (i + 1).toString();
+      document.getElementById(rests).innerHTML = result.rests[i];
+      
+      // Votes
+      votes = 'vote' + (i + 1).toString();
+      document.getElementById(votes).innerHTML = result.votes[i].toString();
     }
   });
   xhr.send(null);
