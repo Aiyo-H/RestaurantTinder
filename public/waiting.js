a4e94e5a2b8e05cf2e42b96378d43cc28d36a828
Glitch (weak-playful-winterberry) <none>
2020-06-07 21:18:41 +0000

    🚆🏨 Checkpoint ./public/waiting.js:13964885/305

--- public/waiting.js
+++ public/waiting.js
@@ -1,4 +1,4 @@
 // NEXT HTML
 document.querySelector("#refbun").addEventListener("click", () => {
-      window.location = "https://weak-playful-winterberry.glitch.me";
+  window.location = "https://weak-playful-winterberry.glitch.me";
 });
